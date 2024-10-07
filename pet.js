5e27797b930778e87116ccb5c074b924582b7b39
mehedi-hasannnn <mhasan2999@gmail.com>
2024-10-07 15:32:09 +0600

    details modal added

--- pet.js
+++ pet.js
@@ -14,6 +14,42 @@ const loadCategories = () => {
       .catch((error) => console.log(error));
   };
 
+  const loadDetails = async(petId) =>{
+    console.log(petId);
+    const url =`https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayDetails(data.petData);
+  };
+
+  const displayDetails =(petData)=>{
+    console.log(petData);
+    const detailContainer = document.getElementById("modal-content");
+
+    detailContainer.innerHTML = `
+    
+    <img class="rounded-lg h-full w-full object-cover"
+        src=${petData.image}
+        />
+
+    <div class="mb-3 p-3">
+        <h3 class="font-bold text-2xl mb-2">${petData.pet_name}<h3/>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/health-data.png" alt="health-data"/> Breed: ${petData.breed} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/calendar--v1.png" alt="calendar--v1"/> Birth: ${petData.date_of_birth} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="15" src="https://img.icons8.com/fluency-systems-regular/50/mercury.png" alt="mercury"/> Gender: ${petData.gender} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="15" src="https://img.icons8.com/fluency-systems-regular/50/mercury.png" alt="mercury"/> Vaccinated Status: ${petData.vaccinated_status} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/us-dollar--v1.png" alt="us-dollar--v1"/> Price: ${petData.price}$ </p>
+        <br>
+        <h3 class="font-bold text-xl">Details Information</h3>
+        <p> ${petData.pet_details} </p>
+
+        </div>
+    
+    `
+
+    document.getElementById("showModalData").click();
+  }
+
 const displaypets = (pets) => {
     const petContainer = document.getElementById("pets");
     pets.forEach((pet) => {
@@ -21,14 +57,27 @@ const displaypets = (pets) => {
         const card = document.createElement("div");
     card.classList = "card card-compact ";
     card.innerHTML = `
-     <figure ">
-        <img
+     <div class="p-3 border rounded-lg">
+     <figure class="">
+        <img class="rounded-lg h-full w-full object-cover"
         src=${pet.image}
-        class="h-full w-full object-cover"
         />
-        </figure>
-        <div class = "px-0 py-2">
-        </div> `;
+     </figure>
+        <div class="mb-3 p-3">
+        <h3 class="font-bold text-2xl mb-2">${pet.pet_name}<h3/>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/health-data.png" alt="health-data"/> Breed: ${pet.breed} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/calendar--v1.png" alt="calendar--v1"/> Birth: ${pet.date_of_birth} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="15" src="https://img.icons8.com/fluency-systems-regular/50/mercury.png" alt="mercury"/> Gender: ${pet.gender} </p>
+        <p class="text-gray-500 flex gap-1"> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/us-dollar--v1.png" alt="us-dollar--v1"/> Price: ${pet.price}$ </p>
+        </div>
+        <hr>
+        <div class="flex justify-around mt-2">
+        <button class="border rounded-lg  p-2"><img width="20" height="20" src="https://img.icons8.com/fluency-systems-regular/50/facebook-like--v1.png" alt="facebook-like--v1"/></button>
+        <button class="text-clr font-bold border rounded-lg p-2">Adopt</button>
+        <button onclick="loadDetails('${pet.petId}')" class="text-clr font-bold border rounded-lg p-2">Details</button>
+        </div>
+     </div>
+        `;
 petContainer.append(card);
     });
 };
@@ -59,3 +108,4 @@ petContainer.append(card);
  
   loadCategories();
   loadPets();
+  
