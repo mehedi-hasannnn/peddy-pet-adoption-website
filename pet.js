35a6056893462a685c5f03f1f74d7539dc5e145d
mehedi-hasannnn <mhasan2999@gmail.com>
2024-10-08 00:49:20 +0600

    sort by category added

--- pet.js
+++ pet.js
@@ -22,6 +22,51 @@ const loadCategories = () => {
     displayDetails(data.petData);
   };
 
+  const showCongrats = (button) => {
+    const congratsContainer = document.getElementById("congratsContent");
+    congratsContainer.innerHTML = `
+    <img class="mx-auto" width="60" height="60" src="https://img.icons8.com/color-glass/48/handshake--v1.png" alt="handshake--v1"/>
+
+    <h2 class="text-4xl text-center font-bold">Congrats</h2>
+    <br>
+    <p class="text-center font-bold text-xl">Adoption process started for your pet</p>
+    <p id="countdown" class="text-7xl font-bold text-center">3</p>
+    `;
+    const myModal = document.getElementById("mymodal");
+    document.getElementById("showCongratsData").click();
+
+    let countdownValue = 3;
+    const countdownElement = document.getElementById("countdown");
+
+    const countdownInterval = setInterval(() => {
+        countdownValue -= 1;
+        countdownElement.textContent = countdownValue;
+
+        if (countdownValue === 0) {
+            clearInterval(countdownInterval);
+            myModal.close();
+            button.disabled = true; 
+    button.classList.add("bg-gray");
+    button.textContent = "Adopted";
+        }
+    }, 1000);
+
+  };
+
+const showPic = (pet) =>{
+  
+    const likedImageContainer = document.getElementById("likedPet");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    
+    <div class="p-5">
+    <img src= ${pet} />
+    </div>
+    
+    `;
+    likedImageContainer.appendChild(div);
+};
+
   const displayDetails =(petData)=>{
     console.log(petData);
     const detailContainer = document.getElementById("modal-content");
@@ -52,6 +97,25 @@ const loadCategories = () => {
 
 const displaypets = (pets) => {
     const petContainer = document.getElementById("pets");
+    petContainer.innerHTML = "";
+
+    if(pets.length ==0){
+      petContainer.innerHTML = `
+      
+      <div class="mx-auto w-[800px] text-center">
+      <img class="mx-auto" src="images/error.webp"/>
+      <br>
+      <h2 class="font-extrabold text-5xl">No Information Available
+      </h2>
+      <br>
+      <p>OOps!! Sorry, There is no birds available here to adopt right now. Please check again later
+      </p>
+      </div>
+      
+      `;
+      return;
+    }
+
     pets.forEach((pet) => {
         console.log(pet);
         const card = document.createElement("div");
@@ -72,8 +136,8 @@ const displaypets = (pets) => {
         </div>
         <hr>
         <div class="flex justify-around mt-2">
-        <button class="border rounded-lg  p-2"><img width="20" height="20" src="https://img.icons8.com/fluency-systems-regular/50/facebook-like--v1.png" alt="facebook-like--v1"/></button>
-        <button class="text-clr font-bold border rounded-lg p-2">Adopt</button>
+        <button id="likeButton" onclick="showPic('${pet.image}')" class="border rounded-lg  p-2"><img width="20" height="20" src="https://img.icons8.com/fluency-systems-regular/50/facebook-like--v1.png" alt="facebook-like--v1"/></button>
+        <button onclick="showCongrats(this)" class="text-clr font-bold border rounded-lg p-2">Adopt</button>
         <button onclick="loadDetails('${pet.petId}')" class="text-clr font-bold border rounded-lg p-2">Details</button>
         </div>
      </div>
@@ -87,25 +151,32 @@ petContainer.append(card);
     const categoryContainer = document.getElementById("categories");
  
     categories.forEach((item) => {
-      // Create a button
-      const button = document.createElement("button");
-      button.classList.add("btn");
- 
-      // Create an image element for the icon
-      const icon = document.createElement("img");
-      icon.src = item.category_icon;  // Use the category_icon from the API
-     
-      icon.classList.add("category-icon");
- 
-      // Add the icon and category text to the button
-      button.appendChild(icon);
-      button.innerHTML += item.category;
- 
+
+      const buttonContainer = document.createElement("div");
+      buttonContainer.innerHTML = `
+      
+      <button onclick="loadCategoryPets('${item.category}')" class="btn">
+      <img src="${item.category_icon}" />
+      ${item.category}
+      
+      
+      
+      </button>
+      
+      `
    
-      categoryContainer.appendChild(button);
+      categoryContainer.appendChild(buttonContainer);
     });
   };
- 
+ const loadCategoryPets = (id) =>{
+  
+  // fetch
+  fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+      .then((res) => res.json())
+      .then((d) => displaypets(d.data))
+      .catch((error) => console.log(error));
+ }
+  
   loadCategories();
   loadPets();
   
